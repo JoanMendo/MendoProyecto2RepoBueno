daa4e6b4a762eff88ecd154025444c9477b73425
@JoanMendo <joan.mendo.7e7@itb.cat>
2025-04-08 14:54:19 +0200

    Añadidos diversos paquetes

--- Assets/Scripts/Nodos/Node.cs
+++ Assets/Scripts/Nodos/Node.cs
@@ -1,13 +1,15 @@
 ﻿
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.Netcode;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
     public Vector2 position; // Posición dentro de la cuadricula
     public bool hasIngredient = false;
-    BoxCollider boxCollider;
+    private BoxCollider boxCollider;
+    private ResourcesSO currentResource;
 
     public void Awake()
     {
@@ -20,15 +22,21 @@ public class Node : MonoBehaviour
     {
         if (recurso != null)
         {
+            //Instancia el modelo del ingrediente
             Vector3 center = new Vector3(boxCollider.bounds.center.x, boxCollider.bounds.max.y, boxCollider.bounds.center.z);
-
             GameObject cilindro = Instantiate(recurso, center, gameObject.transform.rotation);
             cilindro.transform.SetParent(gameObject.transform, true);
+            if (NetworkManager.Singleton.IsServer)
+            {
+                cilindro.GetComponent<Renderer>().material.color = Color.white;
+            }
+            else if (NetworkManager.Singleton.IsClient)
+            {
+                cilindro.GetComponent<Renderer>().material.color = Color.red;
+            }
 
-
-            Application.Quit();
-
-
+            //Añade el ResourceSO del ingrediente
+            currentResource = recurso.GetComponent<ResourcesSO>();
         }
     }
 
